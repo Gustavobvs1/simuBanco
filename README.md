d47d4f4d5313058a9a83fafa6344f094fe6d96c5
Gustavobvs1 <gustavobvs06@gmail.com>
2023-06-29 17:07:12 -0300

    Criando uma base

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projeto simuBanco
+
+## Esse projeto tem o intuito de ser um programa de simulação de um banco comercial
+
+### Projeto criado com reactJS, react Native e NodeJS
---
+++ frontend/src/Router.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
